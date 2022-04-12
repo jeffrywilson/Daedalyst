e26cc801f38b38fe0ef32aef40aa9da7e8f5eb59
tomgroup <conda0905@gmail.com>
2022-04-12 07:51:33 -0700

    Update index.tsx

--- src/pages/statics/index.tsx
+++ src/pages/statics/index.tsx
@@ -39,9 +39,10 @@ const Statics = () => {
   const [_token, setToken] = useState("SOL");
   const [_day, setDay] = useState(7);
   const [_times, setTimes] = useState();
-  const [_prices, setPrices] = useState();
+  const [_prices, setPrices] = useState<Array<Number>>([]);
 
   useEffect(() => {
+    setPrices([]);    
     const solData = async () => {
       const response = await fetch(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=${_token}&tsym=USD&limit=${_day}&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146`);
       
